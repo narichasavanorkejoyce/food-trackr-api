9db4ebc872eb101d6cfc6d38024aae23057694f3
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-27 13:43:05 -0400

    Added one-to-many relationship between User and Item
    A user has many items.
    An item belongs to a user.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  # validates :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
   include Authentication
-  has_many :examples
+  # has_many :examples
+  has_many :items
+
+  validates :food_name, presence: true
+  validates :purchase_date, presence: true
+  validates :exp_date, presence: true
+  validates :store_name, presence: true
 end
