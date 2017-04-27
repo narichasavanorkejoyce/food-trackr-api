68184ca2d4f763f9e73ef4ae000153fd3177ad26
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-27 11:03:44 -0400

    Added comments from Chris' talk on `current_user`
    I should be using the ProtectedController, since we want all of
    our app actions to be authenticated.

--- app/controllers/open_read_controller.rb
+++ app/controllers/open_read_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 # Inherit from this class to allow unauthenticate access to read actions
 class OpenReadController < ProtectedController
+  # we are going to need to authenticate actions, unless we're
+  # indexing or showing
   READ_ACTIONS = [:index, :show].freeze
   skip_before_action :authenticate, only: READ_ACTIONS
   before_action :set_current_user, only: READ_ACTIONS
--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 # Inherit from this class to require authenticated access to all actions
 class ProtectedController < ApplicationController
+  # this will require authentication for ALL actions
+  # I'll want to use ProtectedController for my app
   before_action :authenticate
 end
