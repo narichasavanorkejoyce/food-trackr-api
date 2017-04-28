c4d433d73399e51f54dc0595dd2390f9eb531ab1
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-28 14:48:24 -0400

    Added validation fields for Item class
    Requires the user to enter a food name, purchase date,
    expiration date, and store name.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  # validates :food_name, presence: true
-  # validates :purchase_date, presence: true
-  # validates :exp_date, presence: true
-  # validates :store_name, presence: true
+  validates :food_name, presence: true
+  validates :purchase_date, presence: true
+  validates :exp_date, presence: true
+  validates :store_name, presence: true
+
 end
