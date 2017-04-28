123055ebd0f1fe23caabb5107e0a1770f0fb5973
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-28 09:35:17 -0400

    Modifications to curl/controller when debugging add item feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,6 +49,6 @@ class ItemsController < ProtectedController
   end
 
   def item_params
-    params.require(:item).permit(:food_name, :purchase_date, :exp_date, :store_name, :quantity, :purchased)
+    params.require(:item).permit(:exp_date, :food_name, :purchase_date, :purchased, :quantity, :store_name)
   end
 end
