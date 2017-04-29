8f27b9412f8a590de499a10435e6cea8863824b5
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-29 12:19:15 -0400

    Per Chris' suggestion, dropped days_to_exp variable from database
    He said that I can calculate this variable in the serializer.
    Still figuring out how to do that...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428172653) do
+ActiveRecord::Schema.define(version: 20170429133728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20170428172653) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "user_id"
-    t.string   "days_to_exp"
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
